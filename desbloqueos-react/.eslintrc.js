4732b8214288ed2273e380b1dd41e772a1dd5a5b
Jonatan Martinez <jmbootcamp@outlook.com>
2021-06-02 11:39:05 -0400

    Changed to .eslintrc.js

---
+++ desbloqueos-react/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-props-no-spreading': [0, {
+      html: 'ignore',
+      custom: 'ignore',
+      explicitSpread: 'ignore',
+    },
+    ],
+    'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
+  },
+};
