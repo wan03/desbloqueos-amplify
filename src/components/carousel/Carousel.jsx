c31bdb6e24526a9c8d1a92fc90fa1b1710399870
adrianelca16 <adrian.elca15@gmail.com>
2023-07-20 22:54:37 +0200

    carousel v1

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,93 +1,20 @@
 import * as React from 'react';
-import { useState } from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
 import { getFeaturedPhones } from '../../shared/api/getPhones';
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
-
 function Carousel() {
-  const phone = getFeaturedPhones();
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = useState(0);
-  const maxSteps = phone.length;
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleStepChange = (step) => {
-    setActiveStep(step);
-  };
+  const phones = getFeaturedPhones();
   return (
-    <Box sx={{ width: '400px', flexGrow: 1 }}>
-      <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-      >
+    <div className="slider-frame">
+      <ul>
         {
-          phone.map((step, index) => (
-            <div key={step.name}>
-              {Math.abs(activeStep - index) <= 2 ? (
-                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                  <Box
-                    component="img"
-                    sx={{
-                      display: 'block',
-                      overflow: 'hidden',
-                      width: '70%',
-                    }}
-                    src={step.imageURL}
-                    alt={step.name}
-                  />
-                </Box>
-              ) : null}
-            </div>
-          ))
-          }
-      </AutoPlaySwipeableViews>
-      <MobileStepper
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        nextButton={(
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        )}
-        backButton={(
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        )}
-      />
-    </Box>
+        phones.map((phone) => (
+          <li key={phone.id}>
+            <img src={phone.imageURL} alt="" />
+          </li>
+        ))
+      }
+      </ul>
+    </div>
   );
 }
 
