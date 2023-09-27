05fbea7010d5781cf763fa422b88cd72961ed0b3
James <jamesdev4you@gmail.com>
2023-09-27 01:46:05 -0400

    premvp beginner section for desktop

--- src/pages/inicio/openingSection/openingSection.jsx
+++ src/pages/inicio/openingSection/openingSection.jsx
@@ -5,17 +5,17 @@ import {
 import WifiIcon from '@mui/icons-material/Wifi';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
 import LocalAtmIcon from '@mui/icons-material/LocalAtm';
-import mujer from '../../../shared/image/17(1).png';
+import mujer from '../../../shared/image/cutbottompic.png';
 import OpeningForm from './openingForm/openingForm';
 import Providers from './providers/providers';
 
 const openingSection = () => (
   <Box sx={{
-    gap: '30px', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', width: '100%', height: '50%', paddingBottom: '250px',
+    gap: '50px', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', width: '100%', height: '50%', paddingBottom: '250px',
   }}
   >
     <Box sx={{
-      display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+      display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', paddingTop: '15px',
     }}
     >
       <Providers />
@@ -23,15 +23,15 @@ const openingSection = () => (
 
     </Box>
     <Box sx={{
-      display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '500px', height: '64vh', gap: '20px',
+      display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '500px', gap: '20px',
     }}
     >
       <Box sx={{
-        display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'start', background: 'linear-gradient(to bottom right ,#091a30, blue)', borderRadius: '10px', width: '500px', height: '80%', position: 'relative', border: '2px solid white', padding: '15px',
+        display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'start', background: 'linear-gradient(to bottom right ,#091a30, blue)', borderRadius: '4%', width: '500px', height: '80%', border: '1px solid white', boxShadow: 'rgba(136, 165, 191, 0.48) 6px 2px 16px 0px, rgba(255, 255, 255, 0.8) -6px -2px 16px 0px',
       }}
       >
         <Box sx={{
-          display: 'flex', flexDirection: 'row', justifyContent: 'center', width: '70%', gap: '35px',
+          display: 'flex', flexDirection: 'row', justifyContent: 'center', width: '70%', gap: '35px', paddingTop: '15px',
         }}
         >
           <Box sx={{
@@ -93,7 +93,7 @@ const openingSection = () => (
           component="img"
           src={mujer}
           sx={{
-            width: '100%', height: '100%', position: 'absolute', bottom: '-102px', left: '0',
+            width: '100%', height: '80%',
           }}
         />
       </Box>
